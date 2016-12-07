a3c4528a721b56d65041d9ed9b60b7b634133995
aquoss <amberquoss@gmail.com>
2016-12-06 18:19:57 -0800

    included ajax and functions to load gifs

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,52 @@
 $(document).on("ready", function(){
 
+  $.ajax({
+    method:'GET',
+    url: 'http://api.giphy.com/v1/gifs/trending',
+    dataType: 'json',
+    data: $('form').serialize(),
+    success: onSuccess,
+    error: onError
+  })
+
+  $('form').on('submit',searchReq);
+  function searchReq(event){
+    event.preventDefault();
+    $('img').remove();
+    $.ajax({
+      method: 'GET',
+      url: 'http://api.giphy.com/v1/gifs/search',
+      dataType: 'json',
+      data: $('form').serialize(),
+      success: onSuccess,
+      error: onError
+    })
+  }
+
+  $('.btn-success').on('click',loadMore);
+  function loadMore(event){
+    $.ajax({
+      method: 'GET',
+      url: 'http://api.giphy.com/v1/gifs/search',
+      dataType: 'json',
+      data: $('form').serialize()+'&offset=25',
+      success: onSuccess,
+      error: onError
+    })
+  }
+
+  function onError(xhr,status,errorThrown){
+    alert("Sorry, there was a problem!");
+    console.log("Error: " + errorThrown);
+    console.log("Status: " + status);
+    console.dir(xhr);
+  }
+
+  function onSuccess(json){
+    var dataArr = json.data;
+    dataArr.forEach(function(object){
+      $('.gif-gallery').append('<img src='+object.images.fixed_height.url+'>');
+    })
+  }
+
 });
